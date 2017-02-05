a891b852554b4afb0b8b34b3c50c54ffb052e7ea
Enzo Frantz <enzo.frantz@goeuro.com>
2017-02-05 23:40:17 +0100

    multiple heart explosion

--- ui/components/Main.js
+++ ui/components/Main.js
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 import styles from '../styles/main.scss';
 import NavigationMenu from './NavigationMenu/NavigationMenu';
 
-export default function animate(event) {
+export default function animate() {
 
-    var floatingHeart = document.createElement('div');
-    floatingHeart.className = "basic-heart floating-heart";
-    document.getElementById("main").appendChild(floatingHeart);
+    var floatingHeartLeft = document.createElement('div');
+    floatingHeartLeft.className = "basic-heart floating-heart-left";
+    document.getElementById("main").appendChild(floatingHeartLeft);
+
+    var floatingHeartMiddle = document.createElement('div');
+    floatingHeartMiddle.className = "basic-heart floating-heart-middle";
+    document.getElementById("main").appendChild(floatingHeartMiddle);
+
+    var floatingHeartRight = document.createElement('div');
+    floatingHeartRight.className = "basic-heart floating-heart-right";
+    document.getElementById("main").appendChild(floatingHeartRight);
 
 }
 
