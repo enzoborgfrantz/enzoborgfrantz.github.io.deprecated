ce24a0d130f7d62cd0ccaf83e8e93fbcb287b232
Enzo Frantz <enzo.frantz@goeuro.com>
2017-02-07 23:17:00 +0100

    no message

--- ui/components/MenuBar/MenuBar.js
+++ ui/components/MenuBar/MenuBar.js
@@ -7,12 +7,12 @@ class MenuBar extends Component {
             <div className="menu-bar">
                 <img className="menu-icon" src="../../ui/assets/icons/google.svg"/>
                 <img className="menu-icon" src="../../ui/assets/icons/facebook.svg"/>
+                <img className="menu-icon" src="../../ui/assets/icons/skype.svg"/>
+                <img className="menu-icon" src="../../ui/assets/icons/whatsapp.svg"/>
                 <img className="menu-icon" src="../../ui/assets/icons/github.svg"/>
                 <img className="menu-icon" src="../../ui/assets/icons/linkedin.svg"/>
-                <img className="menu-icon" src="../../ui/assets/icons/skype.svg"/>
                 <img className="menu-icon" src="../../ui/assets/icons/spotify.svg"/>
                 <img className="menu-icon" src="../../ui/assets/icons/twitter.svg"/>
-                <img className="menu-icon" src="../../ui/assets/icons/whatsapp.svg"/>
             </div>
         );
     };
